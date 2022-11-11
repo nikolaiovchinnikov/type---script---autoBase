4fab19699f12f80c584fd9c32a2ac7aab3bbe5eb
Ovchinnikov Nikolay <slavanikolu.gmail.com>
2022-11-12 00:18:14 +0600

    рефакторинг  stringMetoda

--- ts/stringMetod.ts
+++ ts/stringMetod.ts
@@ -1,21 +1,20 @@
 import { str, } from "./TYPE";
 
 export const getString = ( stateList:any):str => {
-    let str = ''
+    let stateString = ''
     for (let i = 1; i < stateList.length; i++) {
         let string = stateList[i][1]
         string += ""
-        let num = stateList[i][0]
-        str += string
-        for (let k = 0; k < (num - string.length ) ; k++) {
-            str += " "
-            if( k === num - string.length - 1){
-                str += stateList[0]
+        let amount = stateList[i][0]
+        stateString += string
+        for (let k = 0; k < (amount - string.length ) ; k++) {
+            stateString += " "
+            if( k === (amount - string.length) - 1){
+                stateString += stateList[0]
             }
         }
     }
-    str += "\n"
-    return str
+    return stateString += "\n"
 }
 
 
